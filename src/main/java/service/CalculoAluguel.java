5aaedfe729455e81276dad2f62edbd85fd0854aa
Rafael Silva <Rafael.ssilva2910@mail.com>
2026-09-08 11:21:57 -0300

    criando metodo para calcular aluguel

--- src/main/java/service/CalculoAluguel.java
+++ src/main/java/service/CalculoAluguel.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package service;
 
-/**
- *
- * @author O SOZINHO
- */
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class CalculoAluguel {
+    public double calcularValor (
+            double precoDiaria,
+            LocalDate dataInicio,
+            LocalDate dataDevolucao){
+        
+        long dias = ChronoUnit.DAYS.between(dataInicio, dataDevolucao);
+        
+        return precoDiaria * dias;
+    }
     
 }
